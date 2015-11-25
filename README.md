e71bbd651adb73dd2284d43d2f668f0f85afb532
Erwan Le Huitouze <erwan.lehuitouze@tactfactory.com>
2015-11-25 20:03:45 +0100

    Update readme Add .rosintall file Update update script

--- README.md
+++ README.md
@@ -8,34 +8,5 @@ Create a directory for the workspace.
 Go to the workspace directory and type in terminal :
 
     mkdir ./src
-    
-    echo "# IT IS UNLIKELY YOU WANT TO EDIT THIS FILE BY HAND,
-    # UNLESS FOR REMOVING ENTRIES.
-    # IF YOU WANT TO CHANGE THE ROS ENVIRONMENT VARIABLES 
-    # USE THE rosinstall TOOL INSTEAD.
-    # IF YOU CHANGE IT, USE rosinstall FOR THE CHANGES TO TAKE EFFECT
-    - git: {local-name: alfred, uri: 'ssh://git@git.tactfactory.com:2222/projetx/alfred.git'}
-    - git: {local-name: alfred_full, uri: 'ssh://git@git.tactfactory.com:2222/projetx/alfred-full.git'}
-    - git: {local-name: rosbuilding_msgs, uri: 'ssh://git@git.tactfactory.com:2222/projetx/rosbuilding-msgs.git'}
-    - git: {local-name: rosbuilding_msgs_java, uri: 'ssh://git@git.tactfactory.com:2222/projetx/rosbuilding-msgs-java.git'}
-    - git: {local-name: rosjava_dynamic_reconfigure, uri: 'https://github.com/Theosakamg/rosjava_dynamic_reconfigure.git'}
-    - git: {local-name: network_wakeonlan, uri: 'ssh://git@git.tactfactory.com:2222/projetx/network-wakeonlan.git'}
-    - git: {local-name: network_zeroconf, uri: 'ssh://git@git.tactfactory.com:2222/projetx/network-zeroconf.git'}
-    - git: {local-name: media_camera_ip_driver, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-camera-ip-driver.git'}
-    - git: {local-name: common_driver, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-driver.git'}
-    - git: {local-name: media_model, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-model.git'}
-    - git: {local-name: media_msgs, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-msgs.git'}
-    - git: {local-name: media_msgs_java, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-msgs-java.git'}
-    - git: {local-name: media_onkyo_ip_driver, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-onkyo-ip-driver.git'}
-    - git: {local-name: media_samsung_ip_driver, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-samsung-ip-driver.git'}
-    - git: {local-name: media_xbmc_ip_driver, uri: 'ssh://git@git.tactfactory.com:2222/projetx/media-xbmc-ip-driver.git'}
-    - git: {local-name: people_common, uri: 'ssh://git@git.tactfactory.com:2222/projetx/people-common.git'}" > ./src/.rosinstall
-
-To install all sources of Alfred, type in terminal :
-
-    cd ./src
-    wstool update
-
-To build all sources of Alfred, go to the root workspace directory and type in terminal :
-
-    catkin_make
+    git clone ssh://git@git.tactfactory.com:2222/projetx/alfred-full.git ./src/alfred_full
+    ./src/alfred_full/update_packages_source.py
--- update_packages_source.py
+++ update_packages_source.py
@@ -4,20 +4,114 @@ import os
 import subprocess
 import sys
 
-os.chdir("./src")
-subprocess.call(['wstool'])
+class UpdateUtils:
+    def __init__(self):
+        self.data = []
 
-os.chdir("..")
-subprocess.call(['genjava_message_artifacts', '-p', 'rosbuilding_msgs', 'media_msgs', 'heater_msgs'])
-subprocess.call(['catkin_make'])
+    def init(self):
+        # Check if current directory is alfred_full, in this case => init
+        if self.is_in_alfred_full():
+            self.prepare()
+        # in this case we are in workspace
+        elif self.is_in_workspace():
+            self.update_workspace()
+        # in this case => error
+        else:
+            self.log(LogLevel.ERROR, 'You are not in a ROS workspace. Exit.')
+            sys.exit()
 
-os.chdir("./src")
+    def prepare(self):
+        self.log(LogLevel.INFO, 'Prepare alfred sources packages...')
+        self.create_symlink('.rosinstall', '../')
+        self.create_symlink(os.path.basename(__file__), '../../')
+            
+        os.chdir('../..')
+        self.update_workspace()
 
-path = os.getcwd()
+    def update_workspace(self):
+        self.log(LogLevel.INFO, 'Update workspace...')
+        
+        if os.path.isfile('devel/setup.sh'):
+            self.shell_source('devel/setup.sh')
+        elif os.environ.get('ROS_PACKAGE_PATH') == None:
+            self.log(LogLevel.ERROR, 'You must source java catkin setup.sh environment script')
+            sys.exit()
+        
+        self.wstool()
+        self.genjava()
+        self.catkin_make()
+        self.update_eclipse()
 
-for fn in os.listdir('.'):
-    dir = path + "/" + fn + "/"
-    print (dir)
-    if not os.path.isfile(dir) and os.path.isfile(dir + "build.gradle"):
-        os.chdir(dir)
-        subprocess.call(['./gradlew', 'cleanEclipse', 'eclipse'])
+    def wstool(self):
+        self.log(LogLevel.INFO, 'Execute wstool')
+        
+        files = os.listdir('src/alfred_full/')
+        for fn in files:
+            if fn.endswith('.rosinstall') and fn != '.rosinstall':
+                subprocess.call(['wstool', 'merge', '-t', 'src', './src/alfred_full/' + fn])
+                
+        subprocess.call(['wstool', 'update', '-t', 'src'])
+
+    def genjava(self):
+        self.log(LogLevel.INFO, 'Execute genjava_message_artifacts')
+        subprocess.call(['genjava_message_artifacts', '-p', 'rosbuilding_msgs', 'media_msgs', 'heater_msgs'])
+
+    def catkin_make(self):
+        self.log(LogLevel.INFO, 'Execute catkin_make')
+        if subprocess.call(['catkin_make', '--directory', os.getcwd()]) != 0:
+            self.log(LogLevel.ERROR, 'catkin_make failed, update aborted')
+            sys.exit()
+
+    def update_eclipse(self):
+        self.log(LogLevel.INFO, 'Update eclipse projects files...')
+        os.chdir('./src')
+
+        path = os.getcwd()
+
+        for fn in os.listdir('.'):
+            dir = path + '/' + fn + '/'
+            print (dir)
+            if not os.path.isfile(dir) and os.path.isfile(dir + 'build.gradle'):
+                os.chdir(dir)
+                subprocess.call(['./gradlew', 'cleanEclipse', 'eclipse'])
+
+    def is_in_alfred_full(self):
+        result = os.getcwd().endswith('src/alfred_full')
+        return result
+
+    def is_in_workspace(self):
+        result = False
+        files = os.listdir(os.getcwd())
+        for fn in files:
+            if fn == 'src':
+                result = True
+                break
+        return result
+    
+    def create_symlink(self, filename, destination):
+        if os.path.isfile(destination + filename):
+            self.log(LogLevel.WARNING, 'File %s already exists, skip' % (filename))
+        else:
+            os.symlink(os.getcwd() + '/' + filename, destination + filename)
+            self.log(LogLevel.INFO, 'Symlink for %s created' % (filename))
+
+    def shell_source(self, script):
+        """Sometime you want to emulate the action of "source" in bash,
+        settings some environment variables. Here is a way to do it."""
+        import subprocess, os
+        pipe = subprocess.Popen(". %s; env" % script, stdout=subprocess.PIPE, shell=True)
+        output = pipe.communicate()[0]
+        env = dict((line.split("=", 1) for line in output.splitlines()))
+        os.environ.update(env)
+    
+    def log(self, level, message):
+        print('%s%s\x1b[0m' % (level, message))
+
+class LogLevel:
+    DEBUG = '\x1b[0m'
+    INFO = '\x1b[1;32m'
+    WARNING = '\x1b[1;33m'
+    ERROR = '\x1b[1;31m'
+
+if __name__ == '__main__':
+    UpdateUtils().init()
